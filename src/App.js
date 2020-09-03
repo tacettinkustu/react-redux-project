75ebf2a328a25f1a548325f7b26b4ab2586f6a33
tacettinkustu <tacettinkustu>
2020-09-03 21:22:54 +0300

    some change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from "react";
-// components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
 import cartItems from "./cart-items";
-// redux stuff
+import {createStore} from "redux";
+
+const initialStore = {
+  count:0,
+};
+
+function reducer(state,action){
+  console.log({state,action});
+  if(action.type === "DECREASE"){
+    return {count:state.count-1};
+  }
+  return state;
+};
+
+const store = createStore(reducer,initialStore);
+store.dispatch({type:"DECREASE"});
+console.log(store.getState());
+
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()}/>
       <CartContainer cart={cartItems} />
     </main>
   );
