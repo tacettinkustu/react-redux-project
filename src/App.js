f413f06ec42fd066ef267a2928b553160064b4ac
tacettinkustu <tacettinkustu>
2020-09-04 11:13:52 +0300

    morning part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
-// components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
 import cartItems from "./cart-items";
-// redux stuff
+import { createStore } from "redux";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
+
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 5,
+};
+
+const store = createStore(reducer, initialStore);
 
 function App() {
-  // cart setup
-
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
       <CartContainer cart={cartItems} />
-    </main>
+    </Provider>
   );
 }
 
